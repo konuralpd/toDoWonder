c267df0b69379c6acfeaab5f4b9baa9b0bec15e0
konuralpd <98783085+konuralpd@users.noreply.github.com>
2022-06-29 03:09:23 +0300

    Initial Commit

---
+++ toDoWonder/toDoWonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoWonderApp.swift
+//  toDoWonder
+//
+//  Created by Mac on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct toDoWonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
